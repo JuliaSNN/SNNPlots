438fedb670d2550b041894732de20a33bd788fff
Alessio <alessio.quaresima@mpi.nl>
2024-09-16 17:10:14 +0200

    formatted

--- src/SNNPlots.jl
+++ src/SNNPlots.jl
@@ -1,36 +1,36 @@
 module SNNPlots
 
-    using Plots
-    using ColorSchemes
-    using LaTeXStrings
-    using Measures
-    using YAML
-    using XLSX
-    using DrWatson
+using Plots
+using ColorSchemes
+using LaTeXStrings
+using Measures
+using YAML
+using XLSX
+using DrWatson
 
 
-    include("load_MPI_palette.jl")
-    include("conversion.jl")
-    include("utils.jl")
-    # include("default_plots.jl")
-    # include("src/utils.jl")
-    # include("src/raster.jl")
-    # include("src/recognition.jl")
-    # include("src/populations.jl")
+include("load_MPI_palette.jl")
+include("conversion.jl")
+include("utils.jl")
+# include("default_plots.jl")
+# include("src/utils.jl")
+# include("src/raster.jl")
+# include("src/recognition.jl")
+# include("src/populations.jl")
 
-    default(fg_legend = :transparent)
-    default(bg_legend=:transparent)
-    default(xguidefontsize=15)
-    default(yguidefontsize=15)
-    default(ytickfontsize=12)
-    default(xtickfontsize=12)
-    default(legend_title_font_halign=:right)
-    default(legend_title_font_pointsize=14)
-    default(legend_font_pointsize=11)
-    default(margins=5Plots.mm)
+default(fg_legend = :transparent)
+default(bg_legend = :transparent)
+default(xguidefontsize = 15)
+default(yguidefontsize = 15)
+default(ytickfontsize = 12)
+default(xtickfontsize = 12)
+default(legend_title_font_halign = :right)
+default(legend_title_font_pointsize = 14)
+default(legend_font_pointsize = 11)
+default(margins = 5Plots.mm)
 
-    export SNNPlots, Plots
-    export plot, plot!
+export SNNPlots, Plots
+export plot, plot!
 
 end
 # include("dataframes.jl")
