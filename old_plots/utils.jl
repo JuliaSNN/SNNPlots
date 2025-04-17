7b105278aad6ffb1c2f6d1cef53d476be6846dea
Alessio Quaresima <aquaresi@pasteur.fr>
2025-04-17 15:43:26 +0200

    Plots cleaned

--- old_plots/utils.jl
+++
@@ -1,9 +0,0 @@
-
-rectangle(w, h, x, y) = Shape(x .+ [0, w, w, 0], y .+ [0, 0, h, h])
-
-function hash_name(sample, text)
-    _name = hash(sample.params |> struct2dict |> tostringdict |> string)
-    _name = string(_name, "_", sample.params.name)
-    _name = _name * "_$text"
-    return _name
-end
